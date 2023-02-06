d86d397502695a839f4c4f4da78ae4e11da5e7a0
jeongwoo-hong <ghdepddn@snu.ac.kr>
2023-02-07 02:29:15 +0900

    23.2.7.1

---
+++ nwitter/src/firebase.js
@@ -0,0 +1,12 @@
+import * as firebase from "firebase/app"
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBZYU37gB4lTyQFQcibTt_yS-b1piKZTXI",
+    authDomain: "nwitter-81cfe.firebaseapp.com",
+    projectId: "nwitter-81cfe",
+    storageBucket: "nwitter-81cfe.appspot.com",
+    messagingSenderId: "71708533393",
+    appId: "1:71708533393:web:1a454350da326c86695530"
+  };
+
+  firebase.initializeApp(firebaseConfig)
