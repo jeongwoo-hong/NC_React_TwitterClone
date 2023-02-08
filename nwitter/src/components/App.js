9fb7e5dd4bb1d7234d09af9263ed52cff379a0c7
jeongwoo-hong <ghdepddn@snu.ac.kr>
2023-02-08 23:39:55 +0900

    23.2.8.2

--- nwitter/src/components/App.js
+++ nwitter/src/components/App.js
@@ -1,10 +1,14 @@
 import React from "react"
 import AppRouter from "./Router";
+import { useState } from "react"
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(true)
   return (
+    <>
     <AppRouter isLoggedIn = {isLoggedIn} />
+    <footer>&copy; Nwitter {new Date().getFullYear()}</footer>
+    </>
   );
 }
 
