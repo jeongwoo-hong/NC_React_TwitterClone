68ea746801c79af8be18109a399670763899fd42
jeongwoo-hong <ghdepddn@snu.ac.kr>
2023-02-08 23:43:57 +0900

    23.2.8.4

--- nwitter/src/components/App.js
+++ nwitter/src/components/App.js
@@ -1,7 +1,7 @@
 import React from "react"
-import AppRouter from "./Router";
+import AppRouter from "components/Router";
 import { useState } from "react"
-import firebase from "../firebase"
+import firebase from "../myBase"
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(true)
